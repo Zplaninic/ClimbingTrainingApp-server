dd2b12efc4eb69dcd4716912454f3fea3cfd2921
Zplaninic <zvonimir.planinic@gmail.com>
2020-05-22 19:38:30 +0200

    add more jest config in file

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,8 @@
-export const setupFilesAfterEnv = ['<rootDir>/test-db-setup.js']
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/test-db-setup.js'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
+  verbose: true,
+  testURL: 'http://localhost/',
+  restoreMocks: true
+}
