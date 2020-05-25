3c026a84260e51f40b82c0d51c116265178de5f7
Zplaninic <zvonimir.planinic@gmail.com>
2020-05-25 16:06:35 +0200

    climbing tests

---
+++ src/resources/climbing/__tests__/route.controllers.spec.js
@@ -0,0 +1,18 @@
+import controllers from '../route.controllers'
+import { isFunction } from 'lodash'
+
+describe('route controllers', () => {
+  test('has crud controllers', () => {
+    const crudMethods = [
+      'getOne',
+      'getMany',
+      'createOne',
+      'removeOne',
+      'updateOne'
+    ]
+
+    crudMethods.forEach(name =>
+      expect(isFunction(controllers[name])).toBe(true)
+    )
+  })
+})
