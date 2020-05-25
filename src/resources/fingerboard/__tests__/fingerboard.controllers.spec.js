e80a8de68c0103815ba842881f0adb290b9eab39
Zplaninic <zvonimir.planinic@gmail.com>
2020-05-25 16:25:18 +0200

    fingerboard tests

---
+++ src/resources/fingerboard/__tests__/fingerboard.controllers.spec.js
@@ -0,0 +1,18 @@
+import controllers from '../fingerboard.controllers'
+import { isFunction } from 'lodash'
+
+describe('route controllers', () => {
+  test('has crud controllers', () => {
+    const crudMethods = [
+      'getOne',
+      'getMany',
+      'createOne',
+      'removeOne',
+      'updateOne'
+    ]
+
+    crudMethods.forEach(name =>
+      expect(isFunction(controllers[name])).toBe(true)
+    )
+  })
+})
